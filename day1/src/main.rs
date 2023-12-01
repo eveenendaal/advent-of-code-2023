a8d17874e1dc75466d424077ca1750ca0fa81798
Eric Veenendaal <eric@ericveenendaal.com>
2023-12-01 21:13:42 +0000

    part 1 of day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
-    println!("Hello, world!");
+    let mut total: i128 = 0;
+    // Read in the test.txt file
+    let input = std::fs::read_to_string("data.txt").unwrap();
+    // Iterate over each line
+    for line in input.lines() {
+        // replace all letters with a |
+        let line = line.replace(|c: char| c.is_alphabetic(), "|");
+        // split on |
+        let line = line.split("|");
+        let mut first = "";
+        let mut last = "";
+        // iterate over all the elements and print them
+        for element in line.clone() {
+            // skip empty elements
+            if element == "" {
+                continue;
+            }
+            // set if first is empty
+            if first == "" {
+                // get first character
+                first = &element[0..1];
+            }
+            // set last to the current element's last character
+            last = &element[element.len() - 1..element.len()];
+        }
+        // concat first and last and convert to int
+        let next: i128 = format!("{}{}", first, last).parse::<i128>().unwrap();
+        // print next
+        println!("next: {}", next);
+        // add to total
+        total += next;
+    }
+    // print total
+    println!("total: {}", total);
 }
