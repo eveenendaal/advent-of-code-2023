95ad5c78e6f63bae814995f3386053fd43c60619
Eric Veenendaal <eric@ericveenendaal.com>
2023-12-08 14:05:35 +0000

    code should be done

--- day8/main_test.go
+++ day8/main_test.go
@@ -2,17 +2,17 @@ package main
 
 import "testing"
 
-// func TestPart1(t *testing.T) {
-// 	test1 := Part1("test1.txt")
-// 	if test1 != 2 {
-// 		t.Errorf("Part1 was incorrect, got: %d, want: %d.", test1, 2)
-// 	}
+func TestPart1(t *testing.T) {
+	test1 := Part1("test1.txt")
+	if test1 != 2 {
+		t.Errorf("Part1 was incorrect, got: %d, want: %d.", test1, 2)
+	}
 
-// 	test2 := Part1("test2.txt")
-// 	if test2 != 6 {
-// 		t.Errorf("Part1 was incorrect, got: %d, want: %d.", test2, 6)
-// 	}
-// }
+	test2 := Part1("test2.txt")
+	if test2 != 6 {
+		t.Errorf("Part1 was incorrect, got: %d, want: %d.", test2, 6)
+	}
+}
 
 func TestPart2(t *testing.T) {
 	test3 := Part2("test3.txt")
