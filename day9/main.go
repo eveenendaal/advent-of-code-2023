0beae5e7ec21c1295f3b848739c71149ea505977
Eric Veenendaal <eric@ericveenendaal.com>
2023-12-09 13:39:49 +0000

    Finishing part 2

--- day9/main.go
+++ day9/main.go
@@ -34,6 +34,31 @@ func getNextValue(input []int) int {
 	return nextValue
 }
 
+func getPreviousValue(input []int) int {
+	diffs := []int{}
+	for i := 0; i < len(input)-1; i++ {
+		current := input[i]
+		next := input[i+1]
+		diffs = append(diffs, next-current)
+	}
+	// If all the values are 0, then we are done
+	allZeros := true
+	for next := range diffs {
+		if next != 0 {
+			allZeros = false
+			break
+		}
+	}
+
+	firstValue := input[0]
+	previousValue := 0
+	if !allZeros {
+		previousValue = firstValue - getPreviousValue(diffs)
+	}
+	fmt.Printf("Input: %v -> %d\n", input, previousValue)
+	return previousValue
+}
+
 func Part1(filePath string) int {
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -43,8 +68,6 @@ func Part1(filePath string) int {
 	defer file.Close()
 
 	total := 0
-	// create output1.txt file to write to
-	outputFile, _ := os.Create("output1.txt")
 
 	// read file line by line
 	scanner := bufio.NewScanner(file)
@@ -60,7 +83,42 @@ func Part1(filePath string) int {
 		}
 
 		nextValue := getNextValue(input)
-		outputFile.Write([]byte(strconv.Itoa(nextValue) + "\n"))
+
+		total += nextValue
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	return total
+}
+
+func Part2(filePath string) int {
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return -1
+	}
+	defer file.Close()
+
+	total := 0
+
+	// read file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// split line on spaces
+		rawInput := strings.Split(line, " ")
+		input := []int{}
+
+		// convert from string to int
+		for _, v := range rawInput {
+			intValue, _ := strconv.Atoi(v)
+			input = append(input, intValue)
+		}
+
+		nextValue := getPreviousValue(input)
 
 		total += nextValue
 	}
@@ -73,6 +131,6 @@ func Part1(filePath string) int {
 }
 
 func main() {
-	result := Part1("data.txt")
-	fmt.Printf("Part 1 answer: %v\n", result)
+	// fmt.Printf("Part 1 answer: %v\n", Part1("data.txt"))
+	fmt.Printf("Part 2 answer: %v\n", Part2("data.txt"))
 }
--- day9/main_test.go
+++ day9/main_test.go
@@ -3,12 +3,15 @@ package main
 import "testing"
 
 func TestPart1(t *testing.T) {
-	result := Part1(("test.txt"))
+	result := Part1("test.txt")
 	if result != 114 {
 		t.Errorf("Part1 was incorrect, got: %d, want: %d.", result, 114)
 	}
 }
 
 func TestPart2(t *testing.T) {
-	// TODO: Write test cases for Part2 function
+	result := Part2("test.txt")
+	if result != 2 {
+		t.Errorf("Part2 was incorrect, got: %d, want: %d.", result, 2)
+	}
 }
