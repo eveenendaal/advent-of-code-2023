28a6bc95454410336b95f593f8ed91e6ccdcbd01
Eric Veenendaal <eric@ericveenendaal.com>
2023-12-11 23:02:27 +0000

    Adding placeholders for day 12 and 13

---
+++ day13/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	// TODO: Write test case for example scenario
+}
+
+func TestPart2(t *testing.T) {
+	// TODO: Write test case for another example scenario
+}
