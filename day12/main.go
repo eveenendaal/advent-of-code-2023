d64b205c3d0df7458e62bededb94b8bc1d82042a
Eric Veenendaal <eric@ericveenendaal.com>
2023-12-12 12:27:07 +0000

    Still figuring out how the solution works

--- day12/main.go
+++ day12/main.go
@@ -22,9 +22,6 @@ func charCount(input string, char byte) int {
 var spacesRegex = regexp.MustCompile(`\.+`)
 
 func validSolution(input string, counts []int) bool {
-	if charCount(input, '?') > 0 {
-		return false
-	}
 	// trim '.' from input
 	input = strings.Trim(input, ".")
 	parts := spacesRegex.Split(input, -1)
@@ -41,11 +38,68 @@ func validSolution(input string, counts []int) bool {
 	return true
 }
 
-func findSolutions(input string, counts []int, remaining int, total int) int {
-	if remaining == 0 {
-		if validSolution(input, counts) {
-			total += 1
+func dp(i, j int, record string, group []int, cache [][]int) int {
+
+	if i >= len(record) {
+		if j < len(group) {
+			return 0
 		}
+		return 1
+	}
+
+	if cache[i][j] != -1 {
+		return cache[i][j]
+	}
+
+	fmt.Printf("i: %d, j: %d, record: %s, group: %v\n", i, j, record, group)
+
+	res := 0
+	if record[i] == '.' {
+		// if character is a dot, we can skip it
+		res = dp(i+1, j, record, group, cache)
+	} else {
+		// if character is a ?, we can skip it
+		if record[i] == '?' {
+			res += dp(i+1, j, record, group, cache)
+		}
+		// if we still have groups left
+		if j < len(group) {
+			count := 0
+			// count the number of consecutive characters from current i
+			for k := i; k < len(record); k++ {
+				// stop if the count is greater then the group, you hit a ., or you hit the count with ?s
+				if count > group[j] || record[k] == '.' || count == group[j] && record[k] == '?' {
+					break
+				}
+				count += 1
+			}
+
+			// If the count matches the group, we can continue
+			if count == group[j] {
+				// if you haven't hit the end of the record, and the next character is not a #
+				if i+count < len(record) && record[i+count] != '#' {
+					res += dp(i+count+1, j+1, record, group, cache)
+				} else {
+					// if you have hit the end of the record, or the next character is a #
+					res += dp(i+count, j+1, record, group, cache)
+				}
+			}
+		}
+	}
+
+	cache[i][j] = res
+	return res
+}
+
+func findSolutions(input string, counts []int, remaining int, unknowns int, total int) int {
+	if unknowns == 0 {
+		if remaining == 0 && unknowns == 0 {
+			// fmt.Printf("Checking: %s\n", input)
+			if validSolution(input, counts) {
+				total += 1
+			}
+		}
+		return total
 	}
 	// find next unknonwn
 	for i := 0; i < len(input); i++ {
@@ -54,8 +108,10 @@ func findSolutions(input string, counts []int, remaining int, total int) int {
 			option1 := input[:i] + "#" + input[i+1:]
 			option2 := input[:i] + "." + input[i+1:]
 
-			total = findSolutions(option1, counts, remaining-1, total)
-			total = findSolutions(option2, counts, remaining, total)
+			if remaining > 0 {
+				total = findSolutions(option1, counts, remaining-1, unknowns-1, total)
+			}
+			total = findSolutions(option2, counts, remaining, unknowns-1, total)
 			break
 		}
 	}
@@ -72,7 +128,16 @@ func parseLine(input string, counts []int) int {
 	remaining := total - on
 
 	fmt.Printf("Input: %s, Unknowns: %d, Remaining: %d, Counts: %v\n", input, unknowns, remaining, counts)
-	solutions := findSolutions(input, counts, remaining, 0)
+	// solutions := findSolutions(input, counts, remaining, unknowns, 0)
+	var cache [][]int
+	for i := 0; i < len(input); i++ {
+		cache = append(cache, make([]int, len(counts)+1))
+		for j := 0; j < len(counts)+1; j++ {
+			cache[i][j] = -1
+		}
+	}
+	solutions := dp(0, 0, input, counts, cache)
+
 	fmt.Printf("Solutions: %d\n", solutions)
 	if solutions == 0 {
 		panic("No solutions found")
@@ -80,7 +145,7 @@ func parseLine(input string, counts []int) int {
 	return solutions
 }
 
-func Part1(filePath string) int {
+func Solve(filePath string, part2 bool) int {
 	file, err := os.Open(filePath)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
@@ -101,7 +166,19 @@ func Part1(filePath string) int {
 		for i, v := range countString {
 			counts[i], _ = strconv.Atoi(v)
 		}
-		total += parseLine(input[0], counts)
+		springInput := input[0]
+
+		if part2 {
+			tempInput := []string{}
+			tempCounts := []int{}
+			for i := 0; i < 5; i++ {
+				tempInput = append(tempInput, springInput)
+				tempCounts = append(tempCounts, counts...)
+			}
+			springInput = strings.Join(tempInput, "?")
+			counts = tempCounts
+		}
+		total += parseLine(springInput, counts)
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -112,5 +189,6 @@ func Part1(filePath string) int {
 }
 
 func main() {
-	fmt.Println("Part 1:", Part1("data.txt"))
+	fmt.Println("Part 1:", Solve("data.txt", false))
+	fmt.Println("Part 2:", Solve("data.txt", true))
 }
--- day12/main_test.go
+++ day12/main_test.go
@@ -3,12 +3,15 @@ package main
 import "testing"
 
 func TestPart1(t *testing.T) {
-	result := Part1("test.txt")
+	result := Solve("test.txt", false)
 	if result != 21 {
 		t.Errorf("Expected 21, got %d", result)
 	}
 }
 
 func TestPart2(t *testing.T) {
-	// TODO: Write test case for another example scenario
+	// result := Solve("test.txt", true)
+	// if result != 525152 {
+	// 	t.Errorf("Expected 525152, got %d", result)
+	// }
 }
