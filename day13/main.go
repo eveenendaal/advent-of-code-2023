e713d712c76b59fb15e8a6e43718f399299241a7
Eric Veenendaal <eric@ericveenendaal.com>
2023-12-13 22:30:28 +0000

    getting started on solution

--- day13/main.go
+++ day13/main.go
@@ -6,6 +6,32 @@ import (
 	"os"
 )
 
+func handlePattern(pattern []string) int {
+	// Find vertical match
+	totalVerticalLines := len(pattern)
+	start := totalVerticalLines - (totalVerticalLines % 2) // It must be an even number
+
+	for i := start; i > 1; i -= 2 {
+		// Top First
+		// Get i number of rows from the top
+		n := i / 2
+		top := pattern[0:n]
+		bottom := pattern[n:i]
+		fmt.Printf("Top: %v\n", top)
+		fmt.Printf("Bottom: %v\n", bottom)
+
+		// Get i number of rows from the bottom
+
+		// Bottom Next
+
+	}
+
+	fmt.Println(pattern)
+	// Build hortizontal lines
+	// Find horizontal match
+	return 0
+}
+
 func Part1(filePath string) int {
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -15,12 +41,20 @@ func Part1(filePath string) int {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
-	}
 
 	total := 0
+	lines := []string{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line != "" {
+			lines = append(lines, line)
+		} else {
+			total += handlePattern(lines)
+			lines = []string{}
+		}
+	}
+	total += handlePattern(lines)
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
