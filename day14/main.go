4bd1d91e261857e6833de6d58bdd3b56983bd74f
Eric Veenendaal <eric@ericveenendaal.com>
2023-12-15 09:08:51 +0100

    Day 15, part 2 done

--- day14/main.go
+++ day14/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -26,13 +28,51 @@ func hash(input string) int {
 	return currentValue
 }
 
-func Part1(filePath string) int {
+type Lens struct {
+	label       string
+	focalLength int
+}
+
+func parseStep(input string) (int, bool, Lens) {
+	addLens := true
+	label := ""
+	if strings.Contains(input, "-") {
+		label = strings.Split(input, "-")[0]
+		addLens = false
+	}
+
+	// only box additions have a focal length
+	focalLength := 0
+	if addLens {
+		parts := strings.Split(input, "=")
+		if len(parts) == 2 {
+			label = parts[0]
+			focalLength, _ = strconv.Atoi(parts[1])
+		}
+	}
+
+	lens := Lens{
+		label:       label,
+		focalLength: focalLength,
+	}
+	boxId := hash(lens.label)
+	return boxId, addLens, lens
+}
+
+// remove removes the given element at index i from the slice
+// remaining elements are shifted to the left
+func remove[E any](slice []E, s int) []E {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func Solve(filePath string, part2 bool) int {
 	file, err := os.Open(filePath)
 	if err != nil {
 		log.Fatalf("failed to open file: %s", err)
 	}
 
 	total := 0
+	boxes := make([][]Lens, 256)
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
@@ -40,7 +80,30 @@ func Part1(filePath string) int {
 		// split string on ,
 		blocks := strings.Split(scanner.Text(), ",")
 		for _, block := range blocks {
-			total += hash(block)
+			if part2 {
+				boxId, addLens, lens := parseStep(block)
+				existing := slices.IndexFunc(boxes[boxId], func(l Lens) bool {
+					return l.label == lens.label
+				})
+
+				if addLens {
+					if existing > -1 {
+						// replace exiting lens with same label
+						boxes[boxId][existing] = lens
+					} else {
+						// add to end of box
+						boxes[boxId] = append(boxes[boxId], lens)
+					}
+				} else {
+					// remove from box if exists
+					if existing > -1 {
+						boxes[boxId] = remove(boxes[boxId], existing)
+					}
+				}
+
+			} else {
+				total += hash(block)
+			}
 		}
 	}
 
@@ -49,9 +112,20 @@ func Part1(filePath string) int {
 		log.Fatalf("failed to close file: %s", err)
 	}
 
-	return total
+	if part2 {
+		total = 0
+		for i, box := range boxes {
+			for j, lens := range box {
+				total += (i + 1) * (j + 1) * lens.focalLength
+			}
+		}
+		return total
+	} else {
+		return total
+	}
 }
 
 func main() {
-	fmt.Printf("Part 1: %d\n", Part1("input.txt"))
+	// fmt.Printf("Part 1: %d\n", Solve("input.txt", false))
+	fmt.Printf("Part 2: %d\n", Solve("input.txt", true))
 }
--- day14/main_test.go
+++ day14/main_test.go
@@ -3,19 +3,22 @@ package main
 import "testing"
 
 func TestPart1Example1(t *testing.T) {
-	result := Part1("test1.txt")
+	result := Solve("test1.txt", false)
 	if result != 52 {
 		t.Errorf("Expected 52, got %d", result)
 	}
 }
 
 func TestPart1Example2(t *testing.T) {
-	result := Part1("test2.txt")
+	result := Solve("test2.txt", false)
 	if result != 1320 {
 		t.Errorf("Expected 1320, got %d", result)
 	}
 }
 
 func TestPart2(t *testing.T) {
-	// TODO: Add your test logic here
+	result := Solve("test2.txt", true)
+	if result != 145 {
+		t.Errorf("Expected 145, got %d", result)
+	}
 }
