229cee57dd128bab08542f3c6c2da6a12befa501
Eric Veenendaal <eric@ericveenendaal.com>
2023-12-15 14:44:46 +0000

    starting working

--- day14/main.go
+++ day14/main.go
@@ -14,12 +14,22 @@ func Solve(filePath string) int {
 	}
 
 	total := 0
+	columns := make([]string, 0)
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
+		// Move each character into a column
+		for i, c := range scanner.Text() {
+			if len(columns) <= i {
+				columns = append(columns, "")
+			}
+			columns[i] += string(c)
+		}
 	}
+
+	fmt.Printf("Columns: %v\n", columns)
 
 	err = file.Close()
 	if err != nil {
