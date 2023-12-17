16ef35c3f3c6c1e49d728c704b237d0b6fc99767
Eric Veenendaal <eric@ericveenendaal.com>
2023-12-17 19:35:00 +0100

    Adding more

--- aoc/main.go
+++ aoc/main.go
@@ -1,9 +1,43 @@
-// base.go
 package base
 
-import "fmt"
+import (
+	"bufio"
+	"log"
+	"os"
+)
 
-// PrintHello prints a hello message.
-func PrintHello() {
-	fmt.Println("Hello from the base library!")
+func ReadFileToLines(filePath string) []string {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	lines := make([]string, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return lines
+}
+
+func ReadFileToCharacters(filePath string) [][]rune {
+	lines := ReadFileToLines(filePath)
+	characters := make([][]rune, len(lines))
+	for y, line := range lines {
+		if len(characters) < y {
+			characters = append(characters, make([]rune, len(line)))
+		}
+		for x, char := range line {
+			characters[y][x] = char
+		}
+	}
+	return characters
 }
