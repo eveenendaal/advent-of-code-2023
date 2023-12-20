3754d1a168ea46cdf4e059f0085a0a8598876236
Eric Veenendaal <eric@ericveenendaal.com>
2023-12-20 08:23:18 +0100

    Day 20 part1, almost there

--- day20/main.go
+++ day20/main.go
@@ -1,10 +1,155 @@
 package main
 
-import aoc "github.com/eveenendaal/advent-of-code-2023/aoc"
+import (
+	"fmt"
+	aoc "github.com/eveenendaal/advent-of-code-2023/aoc"
+	"strings"
+)
+
+type ModuleType int
+
+const (
+	broadcaster ModuleType = iota
+	flipFlop
+	conjunction
+)
+
+type Module struct {
+	name         string
+	moduleType   ModuleType
+	destinations []string
+	state        bool
+	history      map[string]PulseType
+}
+
+type ModuleState int
+type PulseType int
+
+const (
+	Low PulseType = iota
+	High
+)
+
+func createModule(name string, moduleType ModuleType, destinations []string) Module {
+	return Module{name: name, moduleType: moduleType, destinations: destinations, state: false, history: make(map[string]PulseType)}
+}
+
+func processLine(input string) Module {
+	// split on " -> "
+	result := strings.Split(input, " -> ")
+
+	// parse the destinations
+	destinationString := result[1]
+	destinations := strings.Split(destinationString, ", ")
+	// trim the spaces
+	for i, destination := range destinations {
+		destinations[i] = strings.TrimSpace(destination)
+	}
+
+	name := result[0]
+	switch name[0] {
+	case '%':
+		return createModule(name[1:], flipFlop, destinations)
+	case '&':
+		return createModule(name[1:], conjunction, destinations)
+	default:
+		return createModule(name, broadcaster, destinations)
+	}
+}
+
+type Pulse struct {
+	source      string
+	destination string
+	value       PulseType
+}
+
+func (p Pulse) String() string {
+	valueString := "Low"
+	if p.value == High {
+		valueString = "High"
+	}
+	return fmt.Sprintf("%s from %s to %s", valueString, p.source, p.destination)
+}
+
+type PulseQueue struct {
+	queue      []Pulse
+	highPulses int
+	lowPulses  int
+}
+
+func (p *PulseQueue) send(source string, destinations []string, value PulseType) {
+	for _, destination := range destinations {
+		if value == High {
+			p.highPulses++
+		} else {
+			p.lowPulses++
+		}
+		pulse := Pulse{source, destination, value}
+		// fmt.Printf("Sending: %v\n", pulse)
+		p.queue = append(p.queue, pulse)
+	}
+}
+
+func (p *PulseQueue) next() Pulse {
+	pulse := p.queue[0]
+	p.queue = p.queue[1:]
+	return pulse
+}
+
+func (p *PulseQueue) len() int {
+	return len(p.queue)
+}
 
 func Part1(filePath string) int {
 	lines := aoc.ReadFileToLines(filePath)
+	modules := make(map[string]Module)
 
-	// Your code goes here
-	return 0 // Replace this with your actual return value
+	for _, line := range lines {
+		module := processLine(line)
+		modules[module.name] = module
+	}
+
+	queue := PulseQueue{queue: make([]Pulse, 0)}
+	buttonPresses := 1000
+	for i := 0; i < buttonPresses; i++ {
+		queue.send("button", []string{"broadcaster"}, Low)
+	}
+
+	for queue.len() > 0 {
+		pulse := queue.next()
+		module := modules[pulse.destination]
+		switch module.moduleType {
+		case broadcaster:
+			// fmt.Printf("Broadcaster: %v -> %v\n", pulse, module)
+			queue.send(module.name, module.destinations, pulse.value)
+		case flipFlop:
+			// fmt.Printf("FlipFlop: %v -> %v\n", pulse, module)
+			if !module.state && pulse.value == High {
+				// do nothing
+			} else if pulse.value == Low {
+				if !module.state {
+					queue.send(module.name, module.destinations, High)
+				}
+				module.state = !module.state
+			} else if pulse.value == High {
+				if module.state {
+					queue.send(module.name, module.destinations, Low)
+				}
+				module.state = !module.state
+			}
+		case conjunction:
+			// fmt.Printf("Conjunction: %v -> %v\n", pulse, module)
+			module.history[pulse.source] = pulse.value
+			pulseValue := Low
+			for _, next := range module.history {
+				if next == Low {
+					pulseValue = High
+					break
+				}
+			}
+			queue.send(module.name, module.destinations, pulseValue)
+		}
+	}
+
+	return queue.highPulses * queue.lowPulses
 }
--- day20/main_test.go
+++ day20/main_test.go
@@ -2,6 +2,16 @@ package main
 
 import "testing"
 
-func TestFunctionName(t *testing.T) {
-	// Your test code goes here
+func TestPart1Test1(t *testing.T) {
+	result := Part1("test1.txt")
+	if result != 32000000 {
+		t.Errorf("Expected 32000000, got %d", result)
+	}
+}
+
+func TestPart1Test2(t *testing.T) {
+	result := Part1("test2.txt")
+	if result != 11687500 {
+		t.Errorf("Expected 11687500, got %d", result)
+	}
 }
