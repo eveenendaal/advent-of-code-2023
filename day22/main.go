3245c7f04dc642d44057cfd7f44c205957ba3588
Eric Veenendaal <eric@ericveenendaal.com>
2023-12-22 17:05:25 +0100

    Parsed input

--- day22/main.go
+++ day22/main.go
@@ -3,13 +3,38 @@ package day22
 import (
 	"fmt"
 	aoc "github.com/eveenendaal/advent-of-code-2023/aoc"
+	"strconv"
+	"strings"
 )
+
+type Brick struct {
+	x   int
+	y   int
+	z   int
+	row int
+}
 
 func Part1(filePath string) int {
 	lines := aoc.ReadFileToLines(filePath)
+	bricks := make([]Brick, 0)
 
-	for _, line := range lines {
-		fmt.Printf("line: %v\n", line)
+	for row, line := range lines {
+		// splits on ~
+		parts := strings.Split(line, "~")
+		for _, part := range parts {
+			// splits on ,
+			coordinates := strings.Split(part, ",")
+			x, _ := strconv.Atoi(coordinates[0])
+			y, _ := strconv.Atoi(coordinates[1])
+			z, _ := strconv.Atoi(coordinates[2])
+			brick := Brick{x, y, z, row}
+			bricks = append(bricks, brick)
+		}
 	}
+
+	for _, brick := range bricks {
+		fmt.Printf("brick: %v\n", brick)
+	}
+
 	return 0
 }
