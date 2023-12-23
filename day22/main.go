6556cec5322e1d609d6fcb59437969827d83cffb
Eric Veenendaal <eric@ericveenendaal.com>
2023-12-23 07:47:07 +0100

    Adding solution

--- day22/main.go
+++ day22/main.go
@@ -1,40 +1,156 @@
 package day22
 
 import (
-	"fmt"
 	aoc "github.com/eveenendaal/advent-of-code-2023/aoc"
+	"slices"
 	"strconv"
 	"strings"
 )
 
-type Brick struct {
-	x   int
-	y   int
-	z   int
-	row int
+func Part1(filename string) int {
+	input := aoc.ReadFileToLines(filename)
+	sortedBricks := getBricks(input)
+	getNumberOfBricksShiftedAfterSettling(sortedBricks)
+	ans1, _ := findBricksToDisintegrate(sortedBricks)
+	return ans1
 }
 
-func Part1(filePath string) int {
-	lines := aoc.ReadFileToLines(filePath)
-	bricks := make([]Brick, 0)
+func Part2(filename string) int {
+	input := aoc.ReadFileToLines(filename)
+	sortedBricks := getBricks(input)
+	getNumberOfBricksShiftedAfterSettling(sortedBricks)
+	_, ans2 := findBricksToDisintegrate(sortedBricks)
+	return ans2
+}
 
-	for row, line := range lines {
-		// splits on ~
+type Brick struct {
+	start Point
+	end   Point
+}
+
+type Point struct {
+	x int
+	y int
+	z int
+}
+
+func getBricks(input []string) []Brick {
+	var bricks []Brick
+	for _, line := range input {
 		parts := strings.Split(line, "~")
+		var digits []int
 		for _, part := range parts {
-			// splits on ,
-			coordinates := strings.Split(part, ",")
-			x, _ := strconv.Atoi(coordinates[0])
-			y, _ := strconv.Atoi(coordinates[1])
-			z, _ := strconv.Atoi(coordinates[2])
-			brick := Brick{x, y, z, row}
-			bricks = append(bricks, brick)
+			digitStrs := strings.Split(part, ",")
+			for _, digitStr := range digitStrs {
+				digit, _ := strconv.Atoi(digitStr)
+				digits = append(digits, digit)
+			}
+
+		}
+		bricks = append(bricks, Brick{
+			start: Point{digits[0], digits[1], digits[2]},
+			end:   Point{digits[3], digits[4], digits[5]},
+		})
+	}
+
+	// sort bricks by z coordinate
+	slices.SortFunc(bricks, func(a, b Brick) int {
+		return a.start.z - b.start.z
+	})
+	return bricks
+}
+
+func findNewZCoordinates(xyPlane [][]int, p1, p2 Point) (int, int) {
+	z1, z2 := 0, 0
+
+	// if x and z coordinates are same, Brick elongates in y dir
+	// note: this condition will also handle if all coordinates are same, ie 1 cubic meter
+	if p1.x == p2.x && p1.z == p2.z {
+		for i := p1.y; i <= p2.y; i++ {
+			if xyPlane[p1.x][i] > z1 {
+				z1 = xyPlane[p1.x][i]
+			}
+		}
+
+		// max value only needs to be incremented by one
+		// as Brick has only width 1 in z dir
+		z1, z2 = z1+1, z1+1
+
+		for i := p1.y; i <= p2.y; i++ {
+			xyPlane[p1.x][i] = z1
+		}
+
+		// if y and z coordinates are same, Brick elongates in x dir
+	} else if p1.y == p2.y && p1.z == p2.z {
+		for i := p1.x; i <= p2.x; i++ {
+			if xyPlane[i][p1.y] > z1 {
+				z1 = xyPlane[i][p1.y]
+			}
+		}
+
+		// max value only needs to be incremented by one
+		// as Brick has only width 1 in z dir
+		z1, z2 = z1+1, z1+1
+
+		// value only needs to be updated by one
+		// as Brick has only width 1 in z dir
+		for i := p1.x; i <= p2.x; i++ {
+			xyPlane[i][p1.y] = z1
+		}
+
+		// if x and y coordinates are same, Brick elongates in z dir
+	} else if p1.x == p2.x && p1.y == p2.y {
+		// as the Brick extends in Z dir, only a single Brick in
+		// the count plane will need to be updated with latest Z coordinate
+		z1 = xyPlane[p1.x][p1.y] + 1
+		z2 = z1 + p2.z - p1.z
+		xyPlane[p1.x][p1.y] = z2
+	}
+
+	return z1, z2
+}
+
+func findBricksToDisintegrate(bricks []Brick) (int, int) {
+	allowed := 0
+	sum := 0
+
+	for i, _ := range bricks {
+		newBrick := make([]Brick, len(bricks))
+		copy(newBrick, bricks)
+		if i == 0 {
+			newBrick = newBrick[1:]
+		} else if i == len(newBrick)-1 {
+			newBrick = bricks[:len(newBrick)-1]
+		} else {
+			newBrick = append(newBrick[:i], newBrick[i+1:]...)
+		}
+
+		changes := getNumberOfBricksShiftedAfterSettling(newBrick)
+		if changes == 0 {
+			allowed++
+		}
+		sum += changes
+
+	}
+	return allowed, sum
+}
+
+func getNumberOfBricksShiftedAfterSettling(bricks []Brick) int {
+	count := 0
+
+	xyPlane := make([][]int, 10)
+	for i, _ := range xyPlane {
+		xyPlane[i] = make([]int, 10)
+	}
+
+	for i, b := range bricks {
+
+		b.start.z, b.end.z = findNewZCoordinates(xyPlane, b.start, b.end)
+
+		if b.start != bricks[i].start || b.end != bricks[i].end {
+			bricks[i] = Brick{b.start, b.end}
+			count++
 		}
 	}
-
-	for _, brick := range bricks {
-		fmt.Printf("brick: %v\n", brick)
-	}
-
-	return 0
+	return count
 }
--- day22/main_test.go
+++ day22/main_test.go
@@ -2,10 +2,34 @@ package day22
 
 import "testing"
 
-func TestPart1(t *testing.T) {
+func TestPart1Test(t *testing.T) {
 	want := 5
 	got := Part1("test.txt")
 	if got != want {
 		t.Errorf("Part1() = %v, want %v", got, want)
+	}
+}
+
+func TestPart1Solution(t *testing.T) {
+	want := 499
+	got := Part1("input.txt")
+	if got != want {
+		t.Errorf("Part1() = %v, want %v", got, want)
+	}
+}
+
+func TestPart2Test(t *testing.T) {
+	want := 7
+	got := Part2("test.txt")
+	if got != want {
+		t.Errorf("Part2() = %v, want %v", got, want)
+	}
+}
+
+func TestPart2Solution(t *testing.T) {
+	want := 95059
+	got := Part2("input.txt")
+	if got != want {
+		t.Errorf("Part2() = %v, want %v", got, want)
 	}
 }
